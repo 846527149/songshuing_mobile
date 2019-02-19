23543b3341a99728f07f9d72336bba1537d4f131
houyongwei <846527149@qq.com>
2019-02-19 14:51:12 +0800

    songshuing_mobile

---
+++ src/main.js
@@ -0,0 +1,16 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import Index from './view/index'
+import router from './router'
+import { XButton } from 'vux'
+import 'lib-flexible/flexible.js'
+Vue.config.productionTip = false
+Vue.component('x-button', XButton)
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: { Index },
+  template: '<Index/>'
+})
---
+++ src/view/index.vue
@@ -0,0 +1,99 @@
+<template>
+  <div>
+    <card>
+      <div slot="content">
+        <swiper :list="bannerlist"></swiper>
+        <grid :show-lr-borders="false">
+          <grid-item :label="item.label" v-for="(item, index) in girdMenuList" :key="index">
+            <img slot="icon" :src="item.icon">
+          </grid-item>
+        </grid>
+      </div>
+    </card>
+    <card>
+      <div slot="content">
+        <div class="remdiv">1</div>
+      </div>
+    </card>
+  </div>
+</template>
+
+<script>
+import { Card } from "vux";
+import { Swiper } from "vux";
+import { Grid, GridItem } from "vux";
+
+export default {
+  components: {
+    Card,
+    Swiper,
+    Grid,
+    GridItem
+  },
+  methods: {},
+  data() {
+    return {
+      girdMenuList: [
+        {
+          label: "健康自测",
+          icon: "./static/images/ic_checkup_report_checkup.png",
+          url: "#"
+        },
+        {
+          label: "慢病管理",
+          icon: "./static/images/ic_checkup_report_contrast.png",
+          url: "#"
+        },
+        {
+          label: "体检定制",
+          icon: "./static/images/ic_checkup_report_healthmanager.png",
+          url: "#"
+        },
+        {
+          label: "报告查询",
+          icon: "./static/images/ic_checkup_report_screening.png",
+          url: "#"
+        }
+      ],
+      bannerlist: [
+        {
+          url: "#:",
+          img:
+            "https://img.zcool.cn/community/0112495544ca5d0000019ae9813de3.jpg@1280w_1l_2o_100sh.jpg",
+          title: "新人专属套餐优惠"
+        },
+        {
+          url: "#:",
+          img:
+            "https://img.zcool.cn/community/015f475544ca5d0000019ae9dd838f.jpg@1280w_1l_2o_100sh.jpg",
+          title: "体检中心系统正式上线"
+        },
+        {
+          url: "#:",
+          img:
+            "https://img.zcool.cn/community/01a8735544ca5d0000019ae9821574.jpg@1280w_1l_2o_100sh.jpg",
+          title: "糖尿病人每日一餐推荐"
+        }
+      ]
+    };
+  }
+};
+</script>
+
+<style lang="less">
+@import "../assets/less/index.less";
+html {
+  font-family: @main_font-family;
+}
+body {
+  background: @main_backgroud;
+  padding: 0;
+  margin: 0;
+}
+.remdiv {
+  height: 150px;
+}
+a {
+  text-decoration: none;
+}
+</style>
